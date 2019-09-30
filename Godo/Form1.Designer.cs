00083bdb0754b069194f73a85a2071b76f68b8a3
Stewart Melville <stewart.melville@framecreates.co.uk>
2019-09-30 10:02:35 +0100

    First Commit

---
+++ Godo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Godo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRandomise = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRandomise
+            // 
+            this.btnRandomise.Location = new System.Drawing.Point(37, 42);
+            this.btnRandomise.Name = "btnRandomise";
+            this.btnRandomise.Size = new System.Drawing.Size(75, 23);
+            this.btnRandomise.TabIndex = 0;
+            this.btnRandomise.Text = "Test";
+            this.btnRandomise.UseVisualStyleBackColor = true;
+            this.btnRandomise.Click += new System.EventHandler(this.BtnRandomise_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(37, 13);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnRandomise);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRandomise;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ Godo/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Godo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Formatting
+            this.Text = "Godo";
+            this.BackColor = Color.DarkRed;
+            this.Location = new Point(300, 300);
+            this.MaximizeBox = false;
+
+            // Create the ToolTip and associate with the Form container.
+            ToolTip toolTip1 = new ToolTip();
+
+            // Set up the delays for the ToolTip.
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            toolTip1.ShowAlways = true;
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnRandomise_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
